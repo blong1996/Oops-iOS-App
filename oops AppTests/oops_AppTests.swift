e1271c40af12a9a31779c9c08baabcf64e0060a8
Brandon Long <blong1996@gmail.com>
2016-05-27 00:20:49 -0400

    New..

---
+++ oops AppTests/oops_AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  oops_AppTests.swift
+//  oops AppTests
+//
+//  Created by User on 5/26/16.
+//  Copyright © 2016 User. All rights reserved.
+//
+
+import XCTest
+@testable import oops_App
+
+class oops_AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
