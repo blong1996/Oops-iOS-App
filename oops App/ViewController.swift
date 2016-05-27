27618df30e801ecfecdfba103fa3e8ede28bf6fc
Brandon Long <blong1996@gmail.com>
2016-05-27 02:30:18 -0400

    ..

--- oops App/ViewController.swift
+++ oops App/ViewController.swift
@@ -19,22 +19,30 @@ class ViewController: UIViewController {
     @IBOutlet weak var usernameField: UITextField!
     
     @IBOutlet weak var passwordField: UITextField!
-    
+	
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        if segue.identifier == "loginSegue" {
+					// Send over Username to Welcome screenu
+			
+        if segue.identifier == "welcomeSeg" {
         let nextVC = segue.destinationViewController as!
             WelcomeViewController
             nextVC.dataToBePassed = usernameField.text
         }
-    
     }
+	@IBAction func userNameLogin(sender: AnyObject) {
+		// login/sign up button pressed, trigger welcome sigue
+		
+		performSegueWithIdentifier("welcomeSeg", sender: nil)
+	}
+    
 }
 extension ViewController: FBSDKLoginButtonDelegate {
 	
 	func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
 		if error == nil {
+			loginButton.readPermissions = ["email"]
 			// if login successful, continue to welcome screen
-			
+			fetchProfile()
 			
 		} else {
 			print(error.localizedDescription)
@@ -43,6 +51,24 @@ extension ViewController: FBSDKLoginButtonDelegate {
 	
 	func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
 		print("logged out successfully!")
+	}
+	
+	func fetchProfile() {
+		let parameters = ["fields": "email, first_name, last_name,"]
+		FBSDKGraphRequest(graphPath: "me", parameters: parameters).startWithCompletionHandler({ (connection, user, requestError) -> Void in
+			
+			if requestError != nil {
+				print(requestError)
+				return
+			}
+			
+			var _ = user["email"] as? String
+			let firstName = user["first_name"] as? String
+			print(firstName)
+	
+			
+			
+		})
 	}
 	
 }
